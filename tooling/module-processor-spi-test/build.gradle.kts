99a755abab04f15c432e303ae93881c079892f62
Jim Marino <jim.marino@gmail.com>
2022-06-22 11:36:22 +0200

    Updates headers

--- tooling/module-processor-spi-test/build.gradle.kts
+++ tooling/module-processor-spi-test/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2021 Microsoft Corporation
+ *  Copyright (c) 2022 Microsoft Corporation
  *
  *  This program and the accompanying materials are made available under the
  *  terms of the Apache License, Version 2.0 which is available at
@@ -9,8 +9,6 @@
  *
  *  Contributors:
  *       Microsoft Corporation - initial API and implementation
- *       SAP SE - Minor fix
- *
  */
 
 plugins {
